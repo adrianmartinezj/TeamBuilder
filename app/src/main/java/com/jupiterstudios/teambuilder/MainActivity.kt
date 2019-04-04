ee2e321d59e174b4e1c08dfb014f4f4d608a2a12
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-04-04 15:26:23 -0700

    Working splash

--- app/src/main/java/com/jupiterstudios/teambuilder/MainActivity.kt
+++ app/src/main/java/com/jupiterstudios/teambuilder/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jupiterstudios.teambuilder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val background = object : Thread() {
+            override fun run(){
+                try{
+                    Thread.sleep(2500)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                } catch(e : Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
