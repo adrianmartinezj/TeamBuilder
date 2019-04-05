659b9398c9a7f816ebf455c3f977b06464f97ee4
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-04-05 14:04:03 -0700

    Hooking up splash to login / aws setup

--- app/src/main/java/com/jupiterstudios/teambuilder/SplashActivity.kt
+++ app/src/main/java/com/jupiterstudios/teambuilder/SplashActivity.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         val background = object : Thread() {
             override fun run(){
                 try{
                     Thread.sleep(2500)
-                    val intent = Intent(baseContext, MainActivity::class.java)
+                    val intent = Intent(baseContext, LoginActivity::class.java)
+                    startActivity(intent);
                 } catch(e : Exception) {
                     e.printStackTrace()
                 }
