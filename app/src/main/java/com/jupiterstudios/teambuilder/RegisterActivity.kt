1c30ff14296533475bae5a96b99958a120a88bd3
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-04-17 15:29:01 -0700

    Add registration screen

--- app/src/main/java/com/jupiterstudios/teambuilder/RegisterActivity.kt
+++ app/src/main/java/com/jupiterstudios/teambuilder/RegisterActivity.kt
@@ -1,12 +1,20 @@
 package com.jupiterstudios.teambuilder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        var submitButton = findViewById<Button>(R.id.register_submit_button);
+        submitButton.setOnClickListener{
+            val intent = Intent(baseContext, AuthenticatorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
